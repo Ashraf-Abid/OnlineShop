61ac52cedd16a8d8baefba7fa0955d94ddcde17e
Ashraful Kalam Abid <abid7ctg@gmail.com>
2021-01-15 10:49:56 +0600

    navbar,Adding product,updating product

---
+++ OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db;
+         
+        public ProductTypesController(ApplicationDbContext db) {
+
+            _db = db;
+        
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+        //create get action method
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //create post action method 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes) 
+        {
+            if (ModelState.IsValid) {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(productTypes);
+        
+        }
+        //create get action method
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null) {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+        //EDIT post action method 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(productTypes);
+
+        }
+
+
+    }
+}
